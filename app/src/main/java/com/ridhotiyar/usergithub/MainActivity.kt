f55c18f2ac049933bd84d86486375c0b2f16949a
ridhotiyar <ridhoimanz@gmail.com>
2022-06-15 10:20:30 +0800

    add binding, icon, and add searchView

--- app/src/main/java/com/ridhotiyar/usergithub/MainActivity.kt
+++ app/src/main/java/com/ridhotiyar/usergithub/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.ridhotiyar.usergithub
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ridhotiyar.usergithub.databinding.ActivityMainBinding
@@ -19,11 +25,42 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.setTitle("GitHub Users")
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         rvUsers = findViewById(R.id.rv_users)
         rvUsers.setHasFixedSize(true)
 
         list.addAll(listUsers)
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.searh).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 
     private val listUsers: ArrayList<Users>
